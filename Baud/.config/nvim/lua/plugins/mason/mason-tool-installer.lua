8b520d86c6235cddcbc6c0edb3ae0ab3343fa3ed
Declan Chidlow <declan.chidlow@proton.me>
2024-01-01 19:27:29 +0800

    Actually remove Mason...

--- Baud/.config/nvim/lua/plugins/mason/mason-tool-installer.lua
+++
@@ -1,7 +0,0 @@
--- Install and upgrades third party tools automatically
--- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
-
-return {
-	"WhoIsSethDaniel/mason-tool-installer.nvim",
-	-- Configured in main Mason config for proper loading order.
-}
