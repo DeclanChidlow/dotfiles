d7f8d51c0c0bee77d184c1f10b0974cddc11122c
Declan Chidlow <declan.chidlow@proton.me>
2024-01-14 09:55:57 +0800

    General update

---
+++ Baud/.config/nvim/lua/plugins/mason/mason-tool-installer.lua
@@ -0,0 +1,7 @@
+-- Install and upgrades third party tools automatically
+-- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
+
+return {
+	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	-- Configured in main Mason config for proper loading order.
+}
---
+++ Baud/.config/nvim/lua/plugins/nvim-web-devicons.lua
@@ -0,0 +1,6 @@
+-- Add icons to many plugins
+-- https://github.com/nvim-tree/nvim-web-devicons
+
+return {
+	"nvim-tree/nvim-web-devicons",
+}
